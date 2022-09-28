8a5966864efc21cbfef68be083fcc2747b21fe5b
王凯 <wangkai-yanfa@allcorehatress.com>
2022-09-28 13:33:46 +0800

    更新demo，增加swagger示例页面

--- kiilin-sensitive-demo/src/main/java/com/kiilin/sensitive/demo/controller/TestController.java
+++ kiilin-sensitive-demo/src/main/java/com/kiilin/sensitive/demo/controller/TestController.java
@@ -2,7 +2,8 @@ package com.kiilin.sensitive.demo.controller;
 
 import com.kiilin.sensitive.core.annotation.Sensitive;
 import com.kiilin.sensitive.demo.entity.UserInfo;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,15 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
 
-    @RequestMapping("/test")
-    public UserInfo test1() {
-        UserInfo userInfo = new UserInfo();
+    @PostMapping("/test")
+    public UserInfo test1(@RequestBody(required = false) UserInfo userInfo) {
+        if (userInfo == null) {
+            userInfo = new UserInfo();
 
-        userInfo.setName("张三");
-        userInfo.setUsername("zhangsan");
-        userInfo.setEmail("zhangsan@qq.com");
-        userInfo.setIdCard("320321199808081234");
-        userInfo.setPhone("13888888888");
+            userInfo.setName("张三");
+            userInfo.setUsername("zhangsan");
+            userInfo.setEmail("zhangsan@qq.com");
+            userInfo.setIdCard("320321199808081234");
+            userInfo.setPhone("13888888888");
+        }
+
         return userInfo;
     }
 }
